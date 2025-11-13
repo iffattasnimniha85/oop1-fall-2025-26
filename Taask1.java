2584e92b21fbc94deb820504813cbbda9421ad16
Mst Iffat Tasnim Niha <iffattasnimniha560@gmail.com>
2025-11-13 23:22:29 +0600

    25-60851-1

---
+++ Taask1.java
@@ -0,0 +1,11 @@
+public class AverageScore {
+    public static void main(String[] args) {
+        double score1 = 85.5;
+        double score2 = 90.0;
+        double score3 = 78.25;
+
+        double average = (score1 + score2 + score3) / 3;
+
+        System.out.printf("Average score: %.2f\n", average);
+    }
+}
