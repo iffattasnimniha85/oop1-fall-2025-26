5b7ea09f6e1ca5ee62f98aae4e39f95bef2520a2
Mst Iffat Tasnim Niha <iffattasnimniha560@gmail.com>
2025-11-14 00:01:23 +0600

    25-60851-1

---
+++ ChatMessageLength.java
@@ -0,0 +1,7 @@
+public class ChatMessageLength {
+    public static void main(String[] args) {
+        String message = "Hello?";
+        int totalCharacters = message.length();
+        System.out.println("Total number of characters: " + totalCharacters);
+    }
+}
