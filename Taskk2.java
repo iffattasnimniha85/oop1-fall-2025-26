76abab2cfb03e597dfd09a34dd1ca1b2ebbfc4b1
Mst Iffat Tasnim Niha <iffattasnimniha560@gmail.com>
2025-11-13 23:25:13 +0600

    25-60851-1

---
+++ Taskk2.java
@@ -0,0 +1,10 @@
+public class DiscountCalculator {
+    public static void main(String[] args) {
+        double originalPrice = 250;
+        double discount = 15; 
+
+        double finalPrice = originalPrice - (originalPrice * discount / 100);
+
+        System.out.printf("Final price after discount:", finalPrice);
+    }
+}
