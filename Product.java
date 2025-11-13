501fb6e17f0dfcc748f55e9abd354c95737ad006
Mst Iffat Tasnim Niha <iffattasnimniha560@gmail.com>
2025-11-13 23:54:52 +0600

    25-60851-1

---
+++ Product.java
@@ -0,0 +1,8 @@
+public class Product {
+    public static void main(String[] args) {
+        String productName = "Laptop";
+        int productID = 123;
+        String productCode = productName + productID;
+        System.out.println("Product Code: " + productCode);
+    }
+}
